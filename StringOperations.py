fe20279bec16102960de4054c545f21f61b2c19b
alex <alehernandezangeles@gmail.com>
2026-03-04 18:21:02 -0800

    Change

--- StringOperations.py
+++ StringOperations.py
@@ -1,12 +1,32 @@
-# Concatenation
+print("Concatenation:")
 print("Hello " + "world!")
 
-# Repetition
+print("Repetition:")
 print("Repeat " * 3)
 
+print("Membership:")
+print("to the" in "Hello to the world")
+print("to THE" in "Hello to the world")
 
-# Membership
+print("Length:")
+print(len ("123456"))
 
+print("Slicing:")
+print("0123456789"[4])
+print("0123456789"[:])
+print("0123456789"[:-3])
+print("0123456789"[3:])
+print("0123456789"[5:8])
+print("0123456789"[0::3])
 
+print("Formatting & Case Conversion:")
+print("Python is a Programming language".lower())
+print("Python is a Programming language".upper())
+print("Python is a Programming language".title())
+print("Python is a Programming language".capitalize())
+print("Python is a Programming language".swapcase())
+print("42".zfill(5))
+
+print("Searching & Replacing:")
 
 
